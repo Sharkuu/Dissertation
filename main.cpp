32d55a11a43064d8c71a6b93c643cb0a90267a52
Sharkuu <sharku@op.pl>
2017-01-04 20:25:53 +0100

    praca

--- main.cpp
+++ main.cpp
@@ -12,10 +12,10 @@
 #include "Functions.h"
 
 int main(int argc, char *argv[]) {
-    Crystal crystal = Crystal(2e4, 3e4, -1e3, 1e3);
+    Crystal crystal = Crystal(1e4, 1e4, -800, 800);
     crystal.startSimulation(1000);
     std::cout << crystal.countElectrons();
     crystal.saveToFile("example.txt");
-
+    //Crystal crystal = Crystal(1e4, 2e4, -1e3, 1e3);
     return 0;
 }
